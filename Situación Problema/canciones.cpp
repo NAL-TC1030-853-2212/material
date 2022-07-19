e4f600398cf41d0fbda0eb4f9b54c4947b14f5b3
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2022-07-19 16:48:38 -0500

    Canciones final

--- Situación Problema/canciones.cpp
+++ Situación Problema/canciones.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <time.h>
 using namespace std;
 
 class Cancion {
@@ -17,6 +18,10 @@ public:
         os << cancion.track << " - " << cancion.artist << " - " << cancion.genre << " - " << cancion.rating;
         return os;
     }
+    // float getRating() {
+    //     return rating;
+    // }
+    friend class ListaReproduccion;
 };
 
 class ListaReproduccion {
@@ -58,11 +63,57 @@ public:
         for (Cancion cancion : canciones)
             cout << cancion << endl;
     }
-    
+    void mayor_calificacion() {
+        Cancion mayor = canciones.at(0);
+        for (Cancion cancion : canciones) {
+            if (cancion.rating > mayor.rating)
+                mayor = cancion;
+        }
+        cout << "La cancion con mayor rating es: " << mayor << endl;
+    }
+    void cancion_aleatoria() {
+        srand(time(NULL));
+        int posicion = rand() % canciones.size();
+        cout << "Cancion al azar: " << canciones.at(posicion) << endl;
+    }
+    void filtro_genero(string genero) {
+        cout << "Filtro por genero: " << genero << endl;
+        for (Cancion cancion : canciones) {
+            // if (cancion.genre == genero) // genero es exactamente igual a cancion.genre
+            if (cancion.genre.find(genero) != string::npos) // genero está contenido dentro de cancion.genre
+                cout << cancion << endl;
+        }
+    }
+    void filtro_calificacion(float rating, int tipo) {
+        // 0 -> menor o igual que
+        // 1 -> mayor o igual que
+        // 2 -> igual
+        cout << "Filtro por calificacion" << endl;
+        for (Cancion cancion : canciones) {
+            switch(tipo) {
+                case 0:
+                    if (cancion.rating <= rating)
+                        cout << cancion << endl;
+                    break;
+                case 1:
+                    if (cancion.rating >= rating)
+                        cout << cancion << endl;
+                    break;
+                case 2:
+                    if (cancion.rating == rating)
+                        cout << cancion << endl;
+                    break;
+            }
+        }
+    }
 };
 
 int main() {
     ListaReproduccion mi_lista{"Mi primera lista"};
     mi_lista.cargar_archivo("top50.csv", true);
     mi_lista.mostrar_canciones();
+    mi_lista.mayor_calificacion();
+    mi_lista.cancion_aleatoria();
+    mi_lista.filtro_genero("pop");
+    mi_lista.filtro_calificacion(2.92, 2);
 }
